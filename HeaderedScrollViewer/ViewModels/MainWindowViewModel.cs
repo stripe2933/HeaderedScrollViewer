b1d4badb59a487559ac8ae38f8adaaca2a192948
stripe2933 <stripe2933@outlook.com>
2022-07-21 14:23:56 +0900

    Change binding target to the ScrollViewer directly, remove VM's property

--- HeaderedScrollViewer/ViewModels/MainWindowViewModel.cs
+++ HeaderedScrollViewer/ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,6 @@ namespace HeaderedScrollViewer.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private string? _currentHeaderText;
-
-        public string? CurrentHeaderText
-        {
-            get => _currentHeaderText;
-            set => this.RaiseAndSetIfChanged(ref _currentHeaderText, value);
-        }
+        
     }
 }
